cd17403a9c2d8f40c8f1639bbedb7cc3fc1df972
TemTem <messinmind@gmail.com>
2018-04-06 02:00:41 +0300

    session with cookies and «remember me» button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save {self.name = name.downcase}
   before_save {self.email = email.downcase}
 
@@ -13,12 +15,34 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-  
-# Returns the hash digest of the given string.
+
+  # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # remembers a user in the db for use in persistent session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Return true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forget a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405081223) do
+ActiveRecord::Schema.define(version: 20180405213013) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180405081223) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "remember_digest"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
