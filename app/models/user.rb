c90fbc1a057ccac1870d9de74714712bb6e25d18
TemTem <messinmind@gmail.com>
2018-04-05 13:05:02 +0300

    Initial commit
    Has user model with tests

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  before_save {self.name = name.downcase}
+  before_save {self.email = email.downcase}
+
+  validates :name, presence: true,
+                   uniqueness: {case_sensetive: false},
+                   length: {minimum: 5, maximum: 255}
+
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+   validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+
+end
