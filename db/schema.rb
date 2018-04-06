a6a22450fe3c2a6d2d33ddaf5a32c1f09a501089
TemTem <12tem@outlook.com>
2018-04-06 18:03:06 +0300

    Admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405213013) do
+ActiveRecord::Schema.define(version: 20180406134519) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180405213013) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
