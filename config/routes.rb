6005cc7177a8e534d3daf440e6223b6947bfa109
TemTem <12tem@outlook.com>
2018-04-09 11:00:25 +0300

    Password reset
    no tests again. I'll do it next time

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   default_url_options :host => "localhost:3000"
-  get 'sessions/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
@@ -11,4 +8,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [ :new, :create, :edit, :update]
 end
